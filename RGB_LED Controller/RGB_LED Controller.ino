8e4d8c438547405367df5b0fedbe2d9ca7bac74c
Collins Nyaigoti <osebenyaigoti@gmail.com>
2024-08-22 14:21:03 +0300

    Added some documentation

--- RGB_LED Controller/RGB_LED Controller.ino
+++ RGB_LED Controller/RGB_LED Controller.ino
@@ -1,26 +1,26 @@
-#include <EEPROM.h> //Library for EEPROM usage
+#include <EEPROM.h> 
 #include <Wire.h> 
-#include <LiquidCrystal.h>
+#include <LiquidCrystal_I2C.h>
 
-int CLK = 32; //Rotary encoder CLK pin
+int CLK = 32;   //Rotary encoder CLK pin
 int DT =  33;   //DT pin
 int SW =  25;   //SW pin
 
 int RED_PIN = 13;      //LED red pin
-int GREEN_PIN = 12;    //green pin
+int GREEN_PIN = 12;    //green 
 int BLUE_PIN = 26;     //blue
 
-int led_onoff = 1;
+int led_onoff = 1;  // LED state (on or off)
 
-static int oldCLK = LOW;   //Variable to store the value of the CLK pin
-static int oldDT = LOW;  //Variable to store the value of the CLK pin
+static int oldCLK = LOW;   // Previous state of the CLK pin
+static int oldDT = LOW;  // Previous state of the DT pin
 
-liquidCrystal_I2C lcd(0x27, 20, 4);  
+LiquidCrystal_I2C lcd(0x27, 20, 4);
 
-int RED, GREEN, BLUE;
-int INTERVAL, DURATION;
+int RED, GREEN, BLUE;    // Variables for RGB values
+int INTERVAL, DURATION;  // Variables for blink interval and duration
 
-int mr, mg, mb, mi, md;
+int mr, mg, mb, mi, md;  // Variables for modified settings
 
 char buf[20];
 
@@ -29,16 +29,16 @@ int menu = 0; // 0=RED, 1=GREEN, 2=BLUE, 3=INTV, 4=DUR, 5=END
 
 
 void setup() {
-  initsetting();
+  initsetting();  // Initialize EEPROM with default settings
   loadsetting();
   lcd.init();   //Initialize LCD
   lcd.backlight();  //Turn on backlight
 
-  pinMode(CLK, INPUT_PULLUP)  //Set CLK as input with internal pullup resistor
-  pinMode(DT, INPUT_PULLUP)   //Set DT as input with internal pullup resistor
-  pinMode(SW, INPUT_PULLUP)   //Set SW as input with internal pullup resistor
-
-  attachInterrupt(digitalPinToInterrupt(DT), rotdial, CHANGE);
+  pinMode(CLK, INPUT_PULLUP);  
+  pinMode(DT, INPUT_PULLUP);   
+  pinMode(SW, INPUT_PULLUP);   
+  
+  attachInterrupt(digitalPinToInterrupt(DT), rotdial, CHANGE);  // Attach interrupt to DT pin
 }
 
 
@@ -52,12 +52,14 @@ void loop() {
 
   currb = digitalRead(SW);
 
+  //If in setup mode, pressing the button advances the menu
   if (mode == 1 && currb == LOW && lastb == HIGH){
     lcd.clear();
     menu = menu + 1;
     delay(50);
   }
 
+  //If in normal mode, pressing it enters setup mode and copies the current settings to temporary variables.
   if(mode == 0 && currb == LOW && lastb == HIGH){
     lcd.clear(); mode = 1; menu = 0; mr = RED; mg = GREEN; mb = BLUE; mi = INTERVAL; md = DURATION;
     delay(50);    
@@ -71,7 +73,10 @@ void loop() {
   delay(10); // this speeds up the simulation
 }
 
+
 void ledlight() {
+  /*Controls the LED by adjusting its brightness based on the RGB values.
+  When the LED is off, the colors are set to maximum */
   if (led_onoff == 1){
     analogWrite(RED_PIN, 255 - RED);
     analogWrite(GREEN_PIN, 255 - GREEN);
@@ -84,13 +89,14 @@ void ledlight() {
 }
 
 void lcdshow(){
+  //Updates the LCD screen based on the current mode and menu selection
   if (mode == 0) {
     lcd.setCursor(0,0);
     sprintf(buf, "==RGB LED LIGHTING==");
     lcd.print(buf);
-    ldc,setCursor(0,1);
+    lcd.setCursor(0,1);
     sprintf(buf, "R:%3d G:%3d B:%3d   ", RED, GREEN, BLUE);
-    lcd.print((buf);
+    lcd.print(buf);
     lcd.setCursor(0,2);
     sprintf(buf, " INTERVAL:%2d.%d(sec) ", INTERVAL / 10, INTERVAL % 10);
     lcd.print(buf);      
@@ -184,6 +190,7 @@ void lcdshow(){
 }
 
 void loadsetting(){
+  //Loads the stored RGB and timing settings from EEPROM
   RED = EEPROM.read(1);
   GREEN = EEPROM.read(2);
   BLUE = EEPROM.read(3);
@@ -192,22 +199,49 @@ void loadsetting(){
 }
 
 void initsetting() {
-  EEPROM.update(1, 255);
-  EEPROM.update(2, 255);
-  EEPROM.update(3, 255);
-  EEPROM.update(4, 10);
-  EEPROM.update(5, 10);
+  //Initializes the EEPROM with default settings (maximum brightness, default timing)
+  EEPROM.write(1, 255);
+  EEPROM.commit();
+
+  EEPROM.write(2, 255);
+  EEPROM.commit();
+
+  EEPROM.write(3, 255);
+  EEPROM.commit();
+
+  EEPROM.write(4,10);
+  EEPROM.commit();
+
+  EEPROM.write(5, 10);
+  EEPROM.commit();
 }
 
 void savesetting(){
-  RED = mr; EEPROM.update(1, RED);
-  GREEN =mg; EEPROM.update(2, GREEN);
-  BLUE = mb; EEPROM.update(3, BLUE);
-  INTERVAL = mi; EEPROM.update(4, INTERVAL);
-  DURATION = md; EEPROM.update(5, DURATION);
+  //Saves the current settings to EEPROM
+  RED = mr;
+  EEPROM.write(1, RED);
+  EEPROM.commit();
+
+  GREEN =mg; 
+  EEPROM.write(2, GREEN);
+  EEPROM.commit();
+
+  BLUE = mb; 
+  EEPROM.write(3, BLUE);
+  EEPROM.commit();
+
+  INTERVAL = mi; 
+  EEPROM.write(4, INTERVAL);
+  EEPROM.commit();
+
+  DURATION = md; 
+  EEPROM.write(5, DURATION);
+  EEPROM.commit();
 }
 
 void rotdial() {
+  /*Handles changes in the rotary encoder's position to adjust the current setting. 
+  The direction of rotation determines whether the values increase or decrease*/
   if (mode == 0) return;
 
   int direct = 0;                 // Direction initialized to 0
